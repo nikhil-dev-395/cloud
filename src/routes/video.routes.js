1b97b79ef1c482f458131595a84ffbb9d8d208b3
nikhil wankhade <nikhilw395@gmail.com>
2024-02-13 18:54:22 +0530

    some changes like deleting videos folder

--- src/routes/video.routes.js
+++ src/routes/video.routes.js
@@ -1,12 +1,4 @@
-// import { upload_videos } from "../controllers/video.controllers.js";
-// import express from "express";
-// import uploadMulter from '../middleware/uploadMulter.js'; // Adjust the path accordingly
 
-// const router = express.Router();
-
-// router.route("/upload_videos", uploadMulter.single("file")).post(upload_videos);
-
-// export { router as videoRouter };
 
 import { upload_videos } from "../controllers/video.controllers.js";
 import express from "express";
